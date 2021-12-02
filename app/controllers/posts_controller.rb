8624c7dec20f0c2ea8683fc9318da1b6961abea3
Rebecca <Beckyrose200@aol.com>
2021-12-02 15:45:37 +0000

    Rerouting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,24 @@ class PostsController < ApplicationController
   end
 
   def create
+    p '//////// CREATE ROUTE'
     @post = Post.create(post_params)
     redirect_to posts_url
   end
 
   def index
+    p '============== INDEX ROUTE'
     @posts = Post.all
+    @image = Image.new(image_params)
+    respond_to do |format|
+      if @image.save
+        format.html { redirect_to @image, notice: "Image was successfully created." }
+        format.json { render :show, status: :created, location: @image }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @image.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
@@ -27,6 +39,10 @@ class PostsController < ApplicationController
 
   def id_params
     p params.require(:id)
+  end
+
+  def image_params
+    params.require(:image).permit(:title, :caption, :image)
   end
 
 end
